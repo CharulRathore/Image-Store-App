1f72c3b7d04a3bc08b01c53f3c3f03cd4920197f
Charul Rathore <129336177+CharulRathore@users.noreply.github.com>
2024-02-14 21:03:52 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,9 +7,3 @@ def test_home_route():
         response = client.get('/')
         assert response.status_code == 200
         assert b"Home Page" in response.data
-    assert b'Login successful' in response.data
-    assert current_user.is_authenticated
-
-    # Clean up test user
-    db.session.delete(test_user)
-    db.session.commit()
